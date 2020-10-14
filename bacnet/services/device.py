e1290c97c853115c0edf576c57b6da96113f4e6a
NubeDev <aidan@enviroservices.com.au>
2020-10-14 18:14:27 +1100

    got it woring

--- bacnet/services/device.py
+++ bacnet/services/device.py
@@ -27,42 +27,19 @@ class Device:
     def get_network(self, device):
         return Network.get_instance().get_network(device.network)
 
-
-    # def get_points(self, device):
-    #     print("device", device)
-    #     dev_url = f"{device.bac_device_ip}:{device.bac_device_port}"
-    #     bac_device_id = device.bac_device_id
-
-    #     network = Network.get_instance().get_network(device.network)
-    #     return network.read(f"{dev_url} device {bac_device_id} objectList")
-
     def get_points(self, device):
         network = self.get_network(device)
         get_url = self.get_url(device)
-    
         dev_url = get_url["dev_url"]
         bac_device_id = get_url["bac_device_id"]
         read = f"{dev_url} device {bac_device_id} objectList"
-        print(222222)
-        print("device", device)
-        print("get_url", get_url)
-        print('network', network)
-        print("read", read)
         return network.read(read)
-        
+
+      # example point/76e9b1e6-4f3e-4391-9aba-93e1881ecfe4/analogInput/1/presentValue  
     def get_point(self, device, obj, obj_instance, prop):
-
-        # address = test_device_url
-        # obj = pnt_type
-        # obj_instance = pnt_id
-        # prop = "presentValue"
-
-        # readObj  = f"{test_device_url} {obj} {obj_instance} {prop}"
-        # read = bacnet.read(readObj)
         get_url = self.get_url(device)
         network = self.get_network(device)
         dev_url = get_url["dev_url"]
-        # bac_device_id = get_url["bac_device_id"]
         read = f"{dev_url} {obj} {obj_instance} {prop}"
         print(read)
 
