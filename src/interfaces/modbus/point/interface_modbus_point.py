5f35016f08e3639da0e7a673eb59c2d5508cbb02
NubeDev <aidan@enviroservices.com.au>
2020-10-19 13:06:04 +1100

    small update

--- src/interfaces/modbus/point/interface_modbus_point.py
+++ src/interfaces/modbus/point/interface_modbus_point.py
@@ -19,9 +19,9 @@ points_attributes = {
     'mod_point_timeout_global': 'mod_point_timeout_global',
     'mod_point_prevent_duplicates': 'mod_point_prevent_duplicates',
     'mod_device_uuid': 'mod_device_uuid',
-    'mod_point_write_ok': 'mod_point_write_ok',
-    'mod_point_fault': 'mod_point_fault',
-    'mod_point_last_poll_timestamp': 'mod_point_last_poll_timestamp',
+    # 'mod_point_write_ok': 'mod_point_write_ok',
+    # 'mod_point_fault': 'mod_point_fault',
+    # 'mod_point_last_poll_timestamp': 'mod_point_last_poll_timestamp',
 
 }
 
@@ -151,28 +151,28 @@ interface_interface_help_device_uuid = {
     'help': f'{THIS}, {_interface_help_device_uuid} is required'
 
 }
-_interface_help_mod_point_write_ok = 'mod_point_write_ok'
-interface_mod_point_write_ok = {
-    'name': _interface_help_mod_point_write_ok,
-    'type': bool,
-    'required': True,
-    'help': f'{THIS}, {_interface_help_mod_point_write_ok} is required'
-
-}
-
-_interface_mod_point_fault = 'mod_point_fault'
-interface_mod_point_fault = {
-    'name': _interface_mod_point_fault,
-    'type': bool,
-    'required': True,
-    'help': f'{THIS}, {_interface_mod_point_fault} is required'
-
-}
-_interface_mod_point_last_poll_timestamp = 'mod_point_last_poll_timestamp'
-interface_mod_point_last_poll_timestamp = {
-    'name': _interface_mod_point_last_poll_timestamp,
-    'type': str,
-    'required': True,
-    'help': f'{THIS}, {_interface_mod_point_last_poll_timestamp} is required'
-
-}
+# _interface_help_mod_point_write_ok = 'mod_point_write_ok'
+# interface_mod_point_write_ok = {
+#     'name': _interface_help_mod_point_write_ok,
+#     'type': bool,
+#     'required': True,
+#     'help': f'{THIS}, {_interface_help_mod_point_write_ok} is required'
+#
+# }
+#
+# _interface_mod_point_fault = 'mod_point_fault'
+# interface_mod_point_fault = {
+#     'name': _interface_mod_point_fault,
+#     'type': bool,
+#     'required': True,
+#     'help': f'{THIS}, {_interface_mod_point_fault} is required'
+#
+# }
+# _interface_mod_point_last_poll_timestamp = 'mod_point_last_poll_timestamp'
+# interface_mod_point_last_poll_timestamp = {
+#     'name': _interface_mod_point_last_poll_timestamp,
+#     'type': str,
+#     'required': True,
+#     'help': f'{THIS}, {_interface_mod_point_last_poll_timestamp} is required'
+#
+# }
