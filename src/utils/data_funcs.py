14184557a7cd54a5f6e79659300499fae350716b
NubeDev <aidan@enviroservices.com.au>
2020-10-20 15:05:15 +1100

    example of differnt pnt types

--- src/utils/data_funcs.py
+++ src/utils/data_funcs.py
@@ -1,20 +1,21 @@
-def num_round(_val: float, places: int) -> float:
-    if places == 0:
-        out = "{:.0f}".format(_val)
+class DataHelpers:
+    def num_round(self, _val: float, places: int) -> float:
+        if places == 0:
+            out = "{:.0f}".format(_val)
+            return out
+        else:
+            return round(_val, places)
+
+    def limit(self, _val: int, _min: int, _max: int) -> int:
+        if _val <= _min:
+            _val = _min
+            return _val
+        elif _val >= _max:
+            _val = _max
+            return _val
+        else:
+            return _val
+
+    def bool_to_int(self, _val: bool) -> int:
+        out = int(_val == True)
         return out
-    else:
-        return round(_val, places)
-
-
-def limit(_val: int, _min: int, _max: int) -> int:
-    if _val <= _min:
-        _val = _min
-        return _val
-    elif _val >= _max:
-        _val = _max
-        return _val
-    else:
-        return _val
-
-# print(num_round(2.562342, 0))
-# print(limit(-111, 0, 100))
