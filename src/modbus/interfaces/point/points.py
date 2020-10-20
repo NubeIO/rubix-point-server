aa6e68d2568c58395136c480a79d3c827720beb9
NubeDev <aidan@enviroservices.com.au>
2020-10-20 20:53:42 +1100

    add ModbusPointUtils

--- src/modbus/interfaces/point/points.py
+++ src/modbus/interfaces/point/points.py
@@ -1,79 +1,51 @@
-import enum
+class ModbusPointUtils:
+    common_point_type = {
+        "readCoils": "readCoils",
+        "readDiscreteInputs": "readDiscreteInputs",
+        "readHoldingRegisters": "readHoldingRegisters",
+        "readInputRegisters": "readInputRegisters",
+        "writeCoil": "writeCoil",
+        "writeRegister": "writeRegister",
+        "writeCoils": "writeCoils",
+        "writeRegisters": "writeRegisters"
+    }
+    common_data_type = {
+        "int16": "int16",
+        "uint16": "uint16",
+        "int32": "int32",
+        "uint32": "uint32",
+        "float": "float",
+        "double": "double",
+    }
+    common_data_endian = {
+        "LEB_BEW": "LEB_BEW",
+        "LEB_LEW": "LEB_LEW",
+        "BEB_LEW": "BEB_LEW",
+        "BEB_BEW": "BEB_BEW"
+    }
 
 
-class ModbusFC(enum.Enum):
-    readCoils = 0
-    readDiscreteInputs = 1,
-    readHoldingRegisters = 2,
-    readInputRegisters = 3,
-    writeCoil = 4,
-    writeRegister = 5,
-    writeCoils = 6,
-    writeRegisters = 7
+class ModbusPointUtilsFuncs:
 
+    @classmethod
+    def common_point_type(cls, _val: str) -> str:
+        for key, value in ModbusPointUtils.common_point_type.items():
+            if _val == value:
+                return _val
+            raise Exception("point type is not correct")
 
-common_point_type = {
-    "readCoils": "readCoils",
-    "readDiscreteInputs": "readDiscreteInputs",
-    "readHoldingRegisters": "readHoldingRegisters",
-    "readInputRegisters": "readInputRegisters",
-    "writeCoil": "writeCoil",
-    "writeRegister": "writeRegister",
-    "writeCoils": "writeCoils",
-    "writeRegisters": "writeRegisters"
-}
+    @classmethod
+    def common_data_type(cls, _val: str) -> str:
 
-common_data_type = {
-    "int16": "int16",
-    "uint16": "uint16",
-    "int32": "int32",
-    "uint32": "uint32",
-    "float": "float",
-    "double": "double",
-}
+        for key, value in ModbusPointUtils.common_data_type.items():
+            if _val == value:
+                return _val
+            raise Exception("data type is not correct")
 
-common_data_endian = {
-    "LEB_BEW": "LEB_BEW",
-    "LEB_LEW": "LEB_LEW",
-    "BEB_LEW": "BEB_LEW",
-    "BEB_BEW": "BEB_BEW"
-}
+    @classmethod
+    def func_common_data_endian(cls, _val: str) -> str:
 
-common_point_enable = {
-    "enable": "enable",
-    "disable": "disable",
-}
-
-# will only send if new value over MQTT
-common_only_send_on_cov = {
-    "enable": True,
-    "disable": False,
-}
-
-common_registerDelay = 30
-
-#
-# def common_point_type(_val: str, _dict: dict) -> str:
-#     for key, value in _dict.items():
-#         if _val == value:
-#             return _val
-#         raise Exception("point type is not correct")
-#
-#
-# def common_data_type(_val: str, _dict: dict) -> str:
-#     for key, value in _dict.items():
-#         if _val == value:
-#             return _val
-#         raise Exception("data type is not correct")
-#
-#
-# def func_common_data_endian(_val: str, _dict: dict) -> str:
-#     for key, value in _dict.items():
-#         if _val == value:
-#             return _val
-#         raise Exception("endian is not correct")
-#
-#
-# val = "LEB_BEW1"
-# test = func_common_data_endian(val, common_data_endian)
-# print(test)
+        for key, value in ModbusPointUtils.common_data_endian.items():
+            if _val == value:
+                return _val
+            raise Exception("endian is not correct")
