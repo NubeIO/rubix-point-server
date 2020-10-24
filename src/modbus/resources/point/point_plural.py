18e7d76f3ec6d2b3064cdbb12b13a44f766c3e6b
Shiny380 <daniel.mckinnell43@gmail.com>
2020-10-24 12:32:26 +1100

    can't test code, sql broken

--- src/modbus/resources/point/point_plural.py
+++ src/modbus/resources/point/point_plural.py
@@ -33,3 +33,41 @@ class ModbusPointPlural(ModbusPointBase):
         _uuid = str(uuid.uuid4())
         data = ModbusPointPlural.parser.parse_args()
         return self.add_point(data, _uuid)
+
+
+from flask_restful import fields
+class ModbusPointPluralPointStore(ModbusPointBase):
+    @marshal_with({'name': fields.String(), 'value': fields.Float()})
+    def get(self, device_uuid):
+        from src import db, ModbusPointStoreModel
+
+        device_points = db.session.query(ModbusPointModel) \
+            .filter(ModbusPointModel.device_uuid == device_uuid) \
+            .subquery()
+
+        partition_table = db.session.query(ModbusPointStoreModel, func.rank()
+                                           .over(order_by=ModbusPointStoreModel.ts.desc(),
+                                                 partition_by=ModbusPointStoreModel.point_uuid)
+                                           .label('rank')) \
+            .subquery()
+        filtered_partition_table = db.session.query(partition_table).filter(partition_table.c.rank == 1).subquery()
+
+        final = db.session.query(device_points.c.name, filtered_partition_table.c.value) \
+            .select_from(device_points) \
+            .join(filtered_partition_table, filtered_partition_table.c.point_uuid == device_points.c.uuid) \
+            .all()
+
+        res = []
+        for row in final:
+            res.append({'name': row.name, 'value': row.value})
+        return res
+
+
+# point_store_get_fields = {
+#     'uuid': fields.String,
+#     'name': fields.String,
+#     'reg': fields.Integer,
+#     'value': fields.Float,
+#     'fault': fields.Boolean
+# }
+# api.add_resource(ModbusPointPluralPointStore, f'/{api_prefix}/modbus/<string:device_uuid>/points_store')
--- src/routes.py
+++ src/routes.py
@@ -7,7 +7,7 @@ from src.modbus.resources.device.device_plural import ModbusDevicePlural
 from src.modbus.resources.device.device_singular import ModbusDeviceSingular
 from src.modbus.resources.network.network_plural import ModusNetworkPlural
 from src.modbus.resources.network.network_singular import ModusNetworkSingular
-from src.modbus.resources.point.point_plural import ModbusPointPlural
+from src.modbus.resources.point.point_plural import ModbusPointPlural, ModbusPointPluralPointStore
 from src.modbus.resources.point.point_singular import ModbusPointSingular
 
 api_prefix = 'api'
@@ -33,4 +33,5 @@ api.add_resource(ModbusDevicePlural, f'/{api_prefix}/modbus/devices')
 api.add_resource(ModbusDeviceSingular, f'/{api_prefix}/modbus/devices/<string:uuid>')
 
 api.add_resource(ModbusPointPlural, f'/{api_prefix}/modbus/points')
+api.add_resource(ModbusPointPluralPointStore, f'/{api_prefix}/modbus/<string:device_uuid>/points_store')
 api.add_resource(ModbusPointSingular, f'/{api_prefix}/modbus/points/<string:uuid>')
