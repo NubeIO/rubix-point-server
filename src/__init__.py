9b045a1ac8376e5b7b6161fc2585593bff2a7062
NubeDev <aidan@enviroservices.com.au>
2020-10-26 19:01:51 +1100

    small fix

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-db_pg = True
+db_pg = False
 if db_pg:
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/bac_rest"
 elif db_pg:
@@ -38,19 +38,19 @@ if not not os.environ.get("WERKZEUG_RUN_MAIN"):
 
     Network.get_instance().start()
 
-    enable_tcp = False
+    enable_tcp = True
     if enable_tcp:
         TcpRegistry.get_instance().register()
         tcp_polling_thread = Thread(target=TcpPolling.get_instance().polling)
         tcp_polling_thread.start()
 
-    enable_rtu = False
+    enable_rtu = True
     if enable_rtu:
         RtuRegistry.get_instance().register()
         rtu_polling_thread = Thread(target=RtuPolling.get_instance().polling)
         rtu_polling_thread.start()
 
-    enable_cleaner = False
+    enable_cleaner = True
     if enable_cleaner:
         point_cleaner_thread = Thread(target=PointStoreCleaner.register)
         point_cleaner_thread.start()
