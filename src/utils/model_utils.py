1d1bb1882853d672b0c8af1913273d28c2419d9f
Shiny380 <dmc@nube-io.com>
2021-01-14 10:05:53 +1100

    point_store timestamp improvement (#170)

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class ModelUtils:
     @staticmethod
     def row2dict(row):
@@ -17,3 +18,14 @@ class ModelUtils:
             attr = getattr(row, column.name)
             d[column.name] = attr
         return d
+
+
+def get_datetime() -> datetime:
+    return datetime.now()
+
+
+def datetime_to_str(datetime_obj: datetime or None) -> str:
+    if datetime_obj is not None:
+        return datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
