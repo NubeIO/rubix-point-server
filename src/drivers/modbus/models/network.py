f6ef58743bba4eaca57e7cf6c5856fcd00e8457b
RaiBnod <binodrayee@gmail.com>
2021-03-09 15:43:35 +0545

    Change modbus schema for network and device

--- src/drivers/modbus/models/network.py
+++ src/drivers/modbus/models/network.py
@@ -1,3 +1,4 @@
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import validates
 
 from src import db
@@ -8,12 +9,22 @@ from src.models.network.model_network_mixin import NetworkMixinModel
 
 class ModbusNetworkModel(NetworkMixinModel):
     __tablename__ = 'modbus_networks'
-    type = db.Column(db.Enum(ModbusType), nullable=False)
+
     rtu_port = db.Column(db.String(80), nullable=True, unique=True)
     rtu_speed = db.Column(db.Integer(), default=9600)
     rtu_stop_bits = db.Column(db.Integer(), default=1)
     rtu_parity = db.Column(db.Enum(ModbusRtuParity), default=ModbusRtuParity.N)
     rtu_byte_size = db.Column(db.Integer(), default=8)
+    tcp_ip = db.Column(db.String(80))
+    tcp_port = db.Column(db.Integer())
+    type = db.Column(db.Enum(ModbusType), nullable=False)
+    timeout = db.Column(db.Integer(), nullable=False, default=3)
+    polling_interval_runtime = db.Column(db.Integer(), default=2)
+    point_interval_ms_between_points = db.Column(db.Integer(), default=30)
+
+    __table_args__ = (
+        UniqueConstraint('tcp_ip', 'tcp_port'),
+    )
 
     @classmethod
     def get_polymorphic_identity(cls) -> Drivers:
@@ -21,24 +32,15 @@ class ModbusNetworkModel(NetworkMixinModel):
 
     @validates('type')
     def validate_type(self, _, value):
-        if isinstance(value, ModbusType):
-            return value
-        if not value or value not in ModbusType.__members__:
-            raise ValueError("Invalid network type")
-        return ModbusType[value]
-
-    @validates('rtu_port')
-    def validate_rtu_port(self, _, value):
-        if not value and self.type == ModbusType.RTU.name:
-            raise ValueError("rtu_port should be be there on type MTU")
-        return value
-
-    @validates('rtu_byte_size')
-    def validate_rtu_byte_size(self, _, value):
-        msg = "rtu_byte_size The number of bits in a byte of serial data. This can be one of 5, 6, 7, or 8. This " \
-              "defaults to 8"
-        if value not in range(5, 9):
-            raise ValueError(msg)
-        elif not value:
-            raise ValueError(msg)
+        if value == ModbusType.RTU.name:
+            if not self.rtu_port:
+                raise ValueError("rtu_port should be be there on type RTU")
+            if not self.rtu_byte_size or self.rtu_byte_size not in range(5, 9):
+                raise ValueError("rtu_byte_size The number of bits in a byte of serial data. This can be one of 5, 6, "
+                                 "7, or 8. This defaults to 8")
+        elif value == ModbusType.TCP.name:
+            if not self.tcp_ip:
+                raise ValueError("tcp_ip should be be there on type TCP")
+            if not self.tcp_port:
+                raise ValueError("tcp_port should be be there on type TCP")
         return value
