29b89f7bdb16fc1a1313f6386cd67eecdee05e27
RaiBnod <binodrayee@gmail.com>
2021-05-04 10:49:21 +0545

    Remove un-necessary code block

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import multiprocessing
 import os
 
 import click
@@ -9,10 +8,6 @@ from src import AppSetting, GunicornFlaskApplication
 
 CLI_CTX_SETTINGS = dict(help_option_names=["-h", "--help"], max_content_width=120, ignore_unknown_options=True,
                         allow_extra_args=True)
-
-
-def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
 
 
 @click.command(context_settings=CLI_CTX_SETTINGS)
@@ -28,17 +23,12 @@ def number_of_workers():
 @click.option('--prod', is_flag=True, help='Production mode')
 @click.option('-s', '--setting-file', help='Rubix Point: setting json file', default=AppSetting.default_setting_file)
 @click.option('-l', '--logging-conf', help='Rubix Point: logging config file')
-@click.option('--workers', type=int, help='Gunicorn: The number of worker processes for handling requests.')
 @click.option('--gunicorn-config', help='Gunicorn: config file(gunicorn.conf.py)')
-@click.option('--log-level', type=click.Choice(['FATAL', 'ERROR', 'WARN', 'INFO', 'DEBUG'], case_sensitive=False),
-              show_default=True, help='Logging level')
-def cli(port, global_dir, data_dir, config_dir, identifier, prod, workers, setting_file, logging_conf, gunicorn_config,
-        log_level):
+def cli(port, global_dir, data_dir, config_dir, identifier, prod, setting_file, logging_conf, gunicorn_config):
     setting = AppSetting(port=port, global_dir=global_dir, data_dir=data_dir, config_dir=config_dir,
                          identifier=identifier, prod=prod).reload(setting_file)
     options = {
         'bind': '%s:%s' % ('0.0.0.0', setting.port),
-        # 'workers': workers if workers is not None else number_of_workers() if prod else 1,
         'workers': 1,
         'logconfig': logging_conf,
         'preload_app': False,
