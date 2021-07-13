d89a3a0d0d663e3743de09f9e923cd757eccc37e
enjuthulung <enjuthulung@gmail.com>
2021-07-13 12:03:04 +0545

    Add pagination on point-store-histories

--- src/resources/rest_schema/schema_point_store_history.py
+++ src/resources/rest_schema/schema_point_store_history.py
@@ -11,3 +11,15 @@ point_store_history_fields = {
     'ts_value': fields.String,
     'ts_fault': fields.String
 }
+
+paginated_point_store_history_fields = {
+    "page": fields.Integer,
+    "per_page": fields.Integer,
+    "pages": fields.Integer,
+    "total": fields.Integer,
+    "has_prev": fields.Boolean,
+    "prev_num": fields.Integer,
+    "has_next": fields.Boolean,
+    "next_num": fields.Integer,
+    "items": fields.Nested(point_store_history_fields)
+}
